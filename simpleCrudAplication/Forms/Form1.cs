e8447eb7339593393e825d2b27e2fece90fdb2c2
Bruno Witt <brunowittn@gmail.com>
2026-01-27 22:25:20 -0300

    feat: estruturação inicial do projeto

---
+++ simpleCrudAplication/Forms/Form1.cs
@@ -0,0 +1,10 @@
+namespace simpleCrudAplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ simpleCrudAplication/Models/Cadastro.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace simpleCrudAplication.Models
+{
+    internal class Cadastro
+    {
+    }
+}
