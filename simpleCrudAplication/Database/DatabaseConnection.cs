676bded2983d778722491165fb33c985ea0194cf
Bruno Witt <brunowittn@gmail.com>
2026-01-27 23:27:34 -0300

    fix: Nome de Database modificado para DatabaseConnection | feat: função de inserir no banco de dados da pasta repositories

---
+++ simpleCrudAplication/Database/DatabaseConnection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Npgsql;
+using DotNetEnv;
+
+
+namespace simpleCrudAplication.Database
+{
+    internal class DatabaseConnection
+    {
+        public static NpgsqlConnection GetConnection()
+        {
+            /*
+            função responsável por criar a conexão com o banco de dados PostgreSQL
+            */
+            string connection =
+                $"Host={Env.GetString("DB_HOST")};" +
+                $"Port={Env.GetString("DB_PORT")};" +
+                $"Username={Env.GetString("DB_USER")};" +
+                $"Password={Env.GetString("DB_PASSWORD")};" +
+                $"Database={Env.GetString("DB_NAME")};";
+
+            return new NpgsqlConnection(connection);
+        }
+    }
+}
---
+++ simpleCrudAplication/Repositories/CadastroRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using simpleCrudAplication.Database;
+using simpleCrudAplication.Models;
+
+namespace simpleCrudAplication.Repositories
+{
+    internal class CadastroRepository
+    {
+        public void Inserir(Cadastro cadastro)
+        {
+            using var connection = DatabaseConnection.GetConnection();
+            connection.Open();
+
+            var query = "INSERT INTO cadastros (texto, numero) VALUES (@texto, @numero);";
+            using var command = new Npgsql.NpgsqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("texto", cadastro.Texto);
+            command.Parameters.AddWithValue("numero", cadastro.Numero);
+
+            command.ExecuteNonQuery();
+        }
+    }
+}
